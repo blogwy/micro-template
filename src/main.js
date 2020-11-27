f5b05592431f7bdc08ac8bc5c88b72b34a73b750
wangyu <wangyu@wangyu.link>
2020-11-27 09:55:10 +0800

    wangyu

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 import moment from 'moment'
-
 import antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 import './assets/style/main.less'
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,16 +1,10 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home">home</div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
